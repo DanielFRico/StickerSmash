4b07c810441ff08eb464b003c09ed33cefaaa7b2
hayderdiaz <hayder.diaz18@gmail.com>
2024-08-23 21:10:56 -0500

    first commit

--- src/screens/RecipeDetails.js
+++ src/screens/RecipeDetails.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View, Button } from "react-native";
 import React from "react";
 
-export const RecipeDetails = () => {
+export const RecipeDetails = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Details Screen</Text>
