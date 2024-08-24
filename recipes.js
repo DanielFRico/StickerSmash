877ae57824d2fb210c969e888e1c3d6f7fb10648
hayderdiaz <hayder.diaz18@gmail.com>
2024-08-24 16:21:59 -0500

    homeact

--- recipes.js
+++ recipes.js
@@ -1,12 +1,65 @@
 export const recipes = [
   {
-    name: "postres tres leches",
-    images: ["one", "two"],
+    id: '1',
+    name: 'Paella',
+    flag: 'https://www.brillante.es/wp-content/uploads/2022/11/paella-mixta.jpg',
+    category: 'Arroces',
+    images: ['one', 'two'],
+    ingredients: ingredientsR1,
+    stars: 4.5,
+    author: authorR1,
+  },
+  {
+    id: '2',
+    name: 'Pasta Marinera',
+    flag: 'https://www.cocinacaserayfacil.net/wp-content/uploads/2023/10/Espaguetis-a-la-marinera.jpg',
+    category: 'Pastas',
+    images: ['one', 'two'],
+    ingredients: ingredientsR1,
+    stars: 5.0,
+    author: authorR1,
+  },
+  {
+    id: '3',
+    name: 'Perro Caliente',
+    flag: 'https://champinonessetasdecuiva.com/wp-content/uploads/2023/08/shutterstock_1009472197-1-1.png',
+    category: 'Comida Rapida',
+    images: ['one', 'two'],
+    ingredients: ingredientsR1,
+    stars: 4.2,
+    author: authorR1,
+  },
+  {
+    id: '4',
+    name: 'Hamburguesa Mixta',
+    flag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfb21WhunqYPT9gnPmFkXdYp7lU_-gd0tcv2L-gNAZn6aiCErTinl231WGqOEhKrTFutk&usqp=CAU',
+    category: 'Comida Rapida',
+    images: ['one', 'two'],
+    ingredients: ingredientsR1,
+    stars: 4.8,
+    author: ['sutanito', 'pepito'],
+  },
+  {
+    id: '5',
+    name: 'Postre de Limon',
+    flag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEOadD1Mhm6DZkxmy9AYdCIQ6XrFSoyUryWw&s',
+    category: 'Postres',
+    images: ['one', 'two'],
+    ingredients: ingredientsR1,
+    stars: 3.8,
+    author: authorR1,
+  },
+  {
+    id: '6',
+    name: 'Postre Tres Leches',
+    flag: 'https://cdn0.recetasgratis.net/es/posts/0/1/9/torta_tres_leches_8910_orig.jpg',
+    category: 'Postres',
+    images: ['one', 'two'],
     ingredients: ingredientsR1,
     stars: 3.5,
     author: authorR1,
   },
 ];
 
-const authorR1 = { name: "pepito", picture: "" };
-const ingredientsR1 = [{ images: ["one", "two"], description: "blablabla" }];
+const authorR1 = { name: 'pepito', picture: "" };
+const ingredientsR1 = [{ images: ['one', 'two'], description: 'blablabla' }];
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,96 @@
-import { View, Text, Button, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, Button, StyleSheet, SafeAreaView, FlatList, Image, TouchableOpacity } from "react-native";
+import React, { startTransition } from "react";
 import { recipes } from "../../recipes";
+
+// Datos de ejemplo
+
+const CountryItem = ({ item, onPress }) => (
+  <TouchableOpacity style={styles.item} onPress={onPress}>
+    <Image source={{ uri: item.flag }} style={styles.flag} />
+    <View style={styles.info}>
+      <Text style={styles.name}>{item.name}</Text>
+      <Text style={styles.capital}>{item.category}</Text>
+      <Text style={styles.start}>
+        Estrellas: {item.stars}
+      </Text>
+      <Text style={styles.author}>
+        Autor: {item.author}
+      </Text>
+    </View>
+  </TouchableOpacity>
+);
+
+
+const renderItem = ({ item }) => (
+  <CountryItem
+    item={item}
+    onPress={() => console.log(`País seleccionado: ${JSON.stringify(item)}`)}
+  />
+);
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+  },
+  listContent: {
+    padding: 16,
+  },
+  item: {
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    padding: 18,
+    marginBottom: 16,
+    alignItems: 'center',
+    shadowColor: '#491010',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  flag: {
+    width: 100,
+    height: 80,
+    marginRight: 16,
+    borderRadius: 4,
+  },
+  info: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 4,
+    color: '#333',
+  },
+  ingredients: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 2,
+  },
+  author: {
+    fontSize: 14,
+    color: '#888',
+  },
+  start: {
+    fontSize: 14,
+    color: '#f1e532',
+  },
+});
+
 
 export const Home = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate("RecipeDetails")}
+    <View style={styles.container}>
+      <FlatList
+        data={recipes}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        contentContainerStyle={styles.listContent}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
